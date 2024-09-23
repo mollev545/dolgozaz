867a3cd212f8d552a51ed320ce7295965eb18107
Molnár Levent <mollev545@hengersor.hu>
2024-09-23 09:15:14 +0200

    first commit

---
+++ elsoprojekt/resources/views/felhasznalo.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Felhasználó</title>
+</head>
+<body>
+<h1>Ez a Felhasználó oldal</h1>
+</body>
+</html>
